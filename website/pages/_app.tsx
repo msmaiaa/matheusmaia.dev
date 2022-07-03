72ef705828038c5b525d90c49ddea61994a5d847
Matheus Maia <roxx77344@gmail.com>
2022-07-03 02:00:45 -0300

    feat: setup redux toolkit base api

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -2,13 +2,17 @@ import { Layout } from "components/layout/Layout";
 import type { AppProps } from "next/app";
 import GlobalStyles from "styles/globalStyles";
 import "../styles/globals.css";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { baseApi } from "api";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
       <Layout>
-        <Component {...pageProps} />
+        <ApiProvider api={baseApi}>
+          <Component {...pageProps} />
+        </ApiProvider>
       </Layout>
     </>
   );
