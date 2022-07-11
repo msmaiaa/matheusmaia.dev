48a4ff861b8b426cb18ac21cdcc0e8aae585c2c1
Matheus Maia <roxx77344@gmail.com>
2022-07-11 12:18:15 -0300

    refactor app startup

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,65 +1,21 @@
 extern crate dotenv;
-use std::sync::Arc;
 
 use dotenv::dotenv;
-use poem::{listener::TcpListener, middleware::Cors, EndpointExt, Route};
-use poem_openapi::OpenApiService;
+use poem::listener::TcpListener;
 
-mod common_types;
-mod config;
-mod controller;
-mod db;
-mod jwt;
-mod prisma;
-mod repository;
-mod service;
-
-use controller::*;
-
-struct Server {
-    port: String,
-    host: String,
-}
-
-impl Server {
-    fn new() -> Server {
-        let host = std::env::var("HOST")
-            .unwrap_or("localhost".to_string())
-            .to_string();
-        let port = std::env::var("PORT")
-            .unwrap_or("3000".to_string())
-            .to_string();
-        Server { port, host }
-    }
-
-    fn get_api_url(&self) -> String {
-        format!("http://{}/api", self.get_server_url())
-    }
-
-    fn get_server_url(&self) -> String {
-        format!("{}:{}", self.host, self.port)
-    }
-}
-
-// TODO: keep the basic stuff here and move the rest to the lib
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     dotenv().ok();
-    let client = db::get_client().await.expect("Failed to get prisma client");
-    let ctx = config::context::Context::new(Arc::new(client));
-    let server = Server::new();
-    let api_service = OpenApiService::new((AuthController, TagController), "Api", "1.0")
-        .server(server.get_api_url());
-    let ui = api_service.swagger_ui();
-    //tracing_subscriber::fmt::init();
-    let app = Route::new()
-        .nest("/api", api_service)
-        .nest("/", ui)
-        .with(Cors::new())
-        //.with(Tracing)
-        .data(ctx);
+    let host = std::env::var("HOST")
+        .unwrap_or("localhost".to_string())
+        .to_string();
+    let port = std::env::var("PORT")
+        .unwrap_or("3000".to_string())
+        .to_string();
 
-    poem::Server::new(TcpListener::bind(server.get_server_url()))
-        .run(app)
-        .await
+    let app = app::App::new(host.clone(), port.clone());
+    let server = app.make_server().await;
+    let listener = TcpListener::bind(format!("{}:{}", host, port));
+    poem::Server::new(listener).run(server).await;
+    Ok(())
 }
